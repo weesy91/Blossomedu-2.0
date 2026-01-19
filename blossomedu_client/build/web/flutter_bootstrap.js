8e8c39e4491c132ff8a5362021739e6559e30b2a
weesy91 <weesy91@gmail.com>
2026-01-19 20:54:21 +0900

    Add built web assets for deployment

--- blossomedu_client/build/web/flutter_bootstrap.js
+++ blossomedu_client/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2721672685"
+    serviceWorkerVersion: "4252544617"
   }
 });
