1c77b88256d904df2ed3d3fab65b3086f04d1ebc
weesy91 <weesy91@gmail.com>
2026-01-23 13:49:44 +0900

    Fix: Adjust input field height to 14.2

--- blossomedu_client/lib/features/teacher/screens/teacher_class_log_create_screen.dart
+++ blossomedu_client/lib/features/teacher/screens/teacher_class_log_create_screen.dart
@@ -1891,7 +1891,7 @@ class _TeacherClassLogCreateScreenState
                   decoration: decoration.copyWith(
                       hintText: rangeHint,
                       contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 14.5)),
+                          horizontal: 10, vertical: 14.2)),
                   controller: TextEditingController(text: row['range'])
                     ..selection = TextSelection.fromPosition(
                         TextPosition(offset: row['range'].length)),
