a533695e43be36c51ae5b2dbc7a8442c0b4b0240
weesy91 <weesy91@gmail.com>
2026-01-26 14:02:32 +0900

    Fix: Remove canLaunchUrl check to resolve iOS/iPad link opening issue

--- blossomedu_client/lib/core/utils/launcher/launcher_io.dart
+++ blossomedu_client/lib/core/utils/launcher/launcher_io.dart
@@ -2,9 +2,12 @@ import 'package:url_launcher/url_launcher.dart';
 
 Future<void> safeLaunch(String url) async {
   final uri = Uri.tryParse(url);
-  if (uri != null && await canLaunchUrl(uri)) {
-    await launchUrl(uri, mode: LaunchMode.externalApplication);
+  if (uri != null) {
+    // [FIX] Skip canLaunchUrl check which fails on iOS if scheme not in Info.plist
+    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+      throw 'Could not launch $url';
+    }
   } else {
-    print('Could not launch $url');
+    throw 'Invalid URL: $url';
   }
 }
